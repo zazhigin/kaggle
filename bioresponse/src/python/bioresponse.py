3b07cca1fcde85ecb80c02376f897a8fe22dcc69
zazhigin <zazhigin@mail.ru>
2016-12-22 14:28:45 +0300

    Biological response gradient boosting classifier.

--- bioresponse/src/python/bioresponse.py
+++ bioresponse/src/python/bioresponse.py
@@ -3,6 +3,11 @@ import pandas as pd
 import numpy as np
 import csv as csv
 from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import f1_score
+from sklearn.metrics import roc_auc_score
 
 # Load training set
 train = pd.read_csv('../../data/train.csv')
@@ -14,6 +19,19 @@ y = np.array(train.values[0::, 0])
 # Gradient boosting classifier
 clf = GradientBoostingClassifier(n_estimators=250, learning_rate=0.2, verbose=False, random_state=241)
 clf.fit(X, y)
+p = clf.predict(X).astype(int)
+
+accuracy = accuracy_score(y, p)
+precision = precision_score(y, p)
+recall = recall_score(y, p)
+f1 = f1_score(y, p)
+roc_auc = roc_auc_score(y, p)
+
+print("accuracy = %.3f" % accuracy)
+print("precision = %.3f" % precision)
+print("recall = %.3f" % recall)
+print("f1 = %.3f" % f1)
+print("roc_auc = %.3f" % roc_auc)
 
 # Load testing set
 test = pd.read_csv('../../data/test.csv')
